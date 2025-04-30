edd99dd61fc03b14c421d64c849014fe3fad9623
Mercy Babayemi <mercyjanet013@gmail.com>
2025-04-30 16:55:55 +0100

    Completed book eventlisteners added(deleteHandler, collectBookTitle, handleSubmit, handleFilterBooks."

--- ReactClass/FirstReactProject/src/components/BookList.jsx
+++ ReactClass/FirstReactProject/src/components/BookList.jsx
@@ -1,17 +1,38 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styles from "./bookList.module.css"
 
 const BookList = () => {
-    const bookList = [
+    const listOfBooks = [
         "Name of the Wind",
         "The Wise Man's Fear",
         "Kafka on the Shore","" +
         "The Master and the Margarita"
     ];
 
-    // const deleteHandler =(item) => {
-    //     bookList.filter((book)=> book !== item);
-    // }
+    const[books, setBooks] = useState(listOfBooks);
+    const[newBook, setNewBook] = useState();
+    const[searchBookValue, setSearchBookValue] = useState("");
+
+    const deleteHandler =(removeBook) => {
+        setBooks(books.filter((book) => (book !== removeBook)))
+    }
+
+    const collectBookTitle = (e) => {
+        setNewBook(e.target.value.trim())
+    }
+
+    const handleSubmit =(e)=>{
+        e.preventDefault();
+        if(newBook){
+            setBooks(prevBook=>[...prevBook, newBook])
+        }
+    }
+
+    const handleFilterBooks =(e)=>{
+        let searchValue = e.target.value.toLowerCase();
+        setSearchBookValue(searchValue)
+    }
+    const filteredBooks = books.filter((book)=> book.toLowerCase().includes(searchBookValue));
 
     return (
         <div id={styles.wrapper}>
@@ -20,15 +41,15 @@ const BookList = () => {
                     <h1 className={styles.title}> Book Collections</h1>
                     <p>Books</p>
                     <form id={styles.searchBooks}>
-                        <input type="text" placeholder="Search books..."/>
+                        <input type="text" placeholder="Search books..." onKeyUp={handleFilterBooks}/>
                     </form>
                 </div>
             </header>
             <div id={styles.bookList}>
-                <h2 className={styles.bookList}>Books to Read</h2>
+                <h2 className={styles.title}>Books to Read</h2>
                 <ul>
                     {
-                        bookList.map((book,index) => (
+                        filteredBooks.map((book,index) => (
                             <li key={index}>
                                 <span className = {styles.name}>{book}</span>
                                 <span className = {styles.delete} onClick={()=> deleteHandler(book)}>delete</span>
@@ -37,8 +58,8 @@ const BookList = () => {
                     }
                 </ul>
             </div>
-            <form id={styles.addBook}>
-                <input type="text" placeholder="Add a book..."/>
+            <form onSubmit={handleSubmit} id={styles.addBook}>
+                <input type="text" placeholder="Add a book..." onChange={collectBookTitle}/>
                 <button>Add</button>
             </form>
 
